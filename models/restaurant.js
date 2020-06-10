f5f405bfb2d5e7fdfbdf8aa689f93fcb3de48e5c
Ty Jenner <tyrant23t@gmail.com>
2020-06-09 21:56:00 -0700

    put together

---
+++ models/restaurant.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+  const Restaurant = sequelize.define("Restaurant", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      lvalidate: {
+        len: [1]
+      }
+    },
+    hours: {
+      type: DataTypes.STRING
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+  Restaurant.associate = function(models) {
+    Restaurant.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    Restaurant.hasMany(models.Menu, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Restaurant;
+};
